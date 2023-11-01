2c4d9f9eb00ade5e0e359f2d215acf15cf35998e
Bluscream <Bluscream@users.noreply.github.com>
2023-11-01 10:44:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program {
+    static void Main(string[] args) {
+        // Hardcoded list of arguments to remove
+        string[] argumentsToRemove = new string[] { "--disable-partition-engine-check" };
+
+        // Remove the arguments to be excluded
+        var filteredArgs = FilterArguments(args, argumentsToRemove);
+
+        // Call the other file with the remaining arguments
+        CallOtherFile(filteredArgs);
+    }
+
+    static string[] FilterArguments(string[] args, string[] argumentsToRemove) {
+        var filteredArgs = new List<string>();
+
+        foreach (var arg in args) {
+            if (!argumentsToRemove.Contains(arg)) {
+                filteredArgs.Add(arg);
+            }
+        }
+
+        return filteredArgs.ToArray();
+    }
+
+    static void CallOtherFile(string[] args) {
+        // Replace "mysqld" with the actual file name and path
+        string fileName = "mysqld";
+
+        // Build the arguments string
+        string arguments = string.Join(" ", args);
+
+        // Start the process
+        Process.Start(fileName, arguments);
+    }
+}
